adbf6f194611af9c6b6c9af91eddd2b516d71282
João Medeiros <jv.medeiros.gallina@gmail.com>
2020-06-30 14:48:50 -0300

    repositories container added and icon usage setup

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,18 +4,53 @@ import React from 'react';
 import logoImg from '../../assets/logo.svg';
 
 // Styles
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
+import Repository from '../repository';
+import { FiChevronRight } from 'react-icons/fi';
 
 const Dashboard: React.FC = () => {
   return (
     <>
+      {/* Header Container */}
       <img src={logoImg} alt="Github Explorer"/>
       <Title>Explore repositórios no GitHub!</Title>
 
+      {/* Search box Container */}
       <Form>
         <input placeholder="Digite o nome do repositório"/>
         <button type="submit">Pesquisar</button>
       </Form>
+
+      {/* Repositories/Page content Container */}
+      <Repositories>
+        <a href="">
+          <img src="https://avatars2.githubusercontent.com/u/53566373?s=460&u=8fcb0053a67405818a52bfcbfdaab00f4eb08d6d&v=4" alt="João Medeiros"/>
+          <div>
+            <strong>Zappts/iClubs</strong>
+            <p>An easy way to help ONGs</p>
+          </div>
+          <FiChevronRight/>
+        </a>
+
+        <a href="">
+          <img src="https://avatars2.githubusercontent.com/u/53566373?s=460&u=8fcb0053a67405818a52bfcbfdaab00f4eb08d6d&v=4" alt="João Medeiros"/>
+          <div>
+            <strong>Zappts/iClubs</strong>
+            <p>An easy way to help ONGs</p>
+          </div>
+          <FiChevronRight/>
+        </a>
+
+        <a href="">
+          <img src="https://avatars2.githubusercontent.com/u/53566373?s=460&u=8fcb0053a67405818a52bfcbfdaab00f4eb08d6d&v=4" alt="João Medeiros"/>
+          <div>
+            <strong>Zappts/iClubs</strong>
+            <p>An easy way to help ONGs</p>
+          </div>
+          <FiChevronRight/>
+        </a>
+
+      </Repositories>
     </>
   );
 };
