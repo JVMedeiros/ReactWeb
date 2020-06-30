4132663148f3ea2a2c549cdb76b6937e14bda40c
João Medeiros <jv.medeiros.gallina@gmail.com>
2020-06-30 14:49:30 -0300

    styles for input, button, icon setup

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
+
 export const Title = styled.h1`
 font-size: 48px;
 color: #3a3a3a;
@@ -43,4 +44,57 @@ button {
     background: ${shade(0.2, `#04d361`)};
   }
 }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+
+    }
+
+    & + a {
+    margin-top: 16px;
+  }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
 `;
