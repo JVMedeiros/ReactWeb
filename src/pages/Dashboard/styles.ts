83b79487d7912202f980a424d3957ed40986c3b4
João Medeiros <jv.medeiros.gallina@gmail.com>
2020-07-12 22:59:02 -0300

    Error tratative style added

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
 
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
 font-size: 48px;
@@ -10,7 +13,7 @@ line-height: 56px;
 
 margin-top: 80px;
 `;
-export const Form = styled.form `
+export const Form = styled.form<FormProps> `
 margin-top: 40px;
 max-width: 700px;
 display: flex;
@@ -22,6 +25,12 @@ input {
   border: 0;
   border-radius: 5px 0 0 5px;
   color: #3a3a3a;
+  border: 2px solid #fff;
+  border-right: 0px;
+
+  ${(props) => props.hasError && css `
+    border-color: #c53030;
+  `}
 
   &::placeholder {
     color: #a8a8b3;
