8cb0e45717956b639f0625202d4489c25365f5ab
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-07-14 22:43:14 -0300

    ESLint

--- src/pages/repository/index.tsx
+++ src/pages/repository/index.tsx
@@ -16,53 +16,53 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-return (
-  <>
-    {/* Logo and back container */}
-    <Header>
-      <img src={logoImg} alt="Github Explorer" />
-      <Link to="/">
-        <FiChevronsLeft size={16} />
-        Voltar
-      </Link>
-    </Header>
+  return (
+    <>
+      {/* Logo and back container */}
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronsLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
 
-    {/* Repository's header */}
-    <RepositoryInfo>
-      <header>
-        <img src="https://avatars0.githubusercontent.com/u/28929274?v=4" alt="RocketSeat" />
-        <div>
-          <strong>RocketSeat/Unform</strong>
-          <p>Descirção do repositório</p>
-        </div>
-      </header>
-      <ul>
-        <li>
-          <strong>1800</strong>
-          <span>Stars</span>
-        </li>
-        <li>
-          <strong>47</strong>
-          <span>Forks</span>
-        </li>
-        <li>
-          <strong>60</strong>
-          <span>Issues abertas</span>
-        </li>
-      </ul>
-    </RepositoryInfo>
+      {/* Repository's header */}
+      <RepositoryInfo>
+        <header>
+          <img src="https://avatars0.githubusercontent.com/u/28929274?v=4" alt="RocketSeat" />
+          <div>
+            <strong>RocketSeat/Unform</strong>
+            <p>Descirção do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1800</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>47</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>60</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
 
-    <Issues>
-      <Link to="huashua" >
-        <div>
-          <strong>BOMDIA</strong>
-          <p>O SOL JÁ NASCEU LÁ NA FAZENDINHA</p>
-        </div>
-        <FiChevronRight size={20} />
-      </Link>
-    </Issues>
-  </>
-);
+      <Issues>
+        <Link to="huashua" >
+          <div>
+            <strong>BOM DIA</strong>
+            <p>O SOL JÁ NASCEU LÁ NA FAZENDINHA</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 
 };
 
